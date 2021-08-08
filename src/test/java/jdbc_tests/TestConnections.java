42c81f7987733fa541a8241fb0425bbc82680c23
Orkhan <jkrogers222@gmail.com>
2021-08-08 12:06:42 -0400

    MetaData

--- src/test/java/jdbc_tests/TestConnections.java
+++ src/test/java/jdbc_tests/TestConnections.java
@@ -1,4 +1,53 @@
 package jdbc_tests;
 
+import java.sql.*;
+
+
 public class TestConnections {
+    public static void main(String[] args) throws SQLException {
+        String dbUrl = "jdbc:oracle:thin:@3.86.27.247:1521:XE";  //
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select * from regions");
+
+        // move to the first row
+        //     resultSet.next();
+
+        // getting info with column name
+        //   System.out.println(resultSet.getString("region_name"));
+
+        // getting info with column name
+        //      System.out.println("resultSet.getInt(1) = " + resultSet.getString(2));
+
+        // print 1- Europe
+        //       2 - Americas
+        //       3 - Asia
+        // System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+
+        // move to second row
+//        resultSet.next();
+//        System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+
+//        resultSet.next();
+//        System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+
+        // printing with loop
+        while(resultSet.next()){
+            System.out.println(resultSet.getInt(1) + " - " + resultSet.getString(2));
+        }
+
+
+
+
+        // close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+
 }
+
+
--- src/test/java/jdbc_tests/jdbc_examples.java
+++ src/test/java/jdbc_tests/jdbc_examples.java
@@ -1,4 +1,105 @@
 package jdbc_tests;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
 public class jdbc_examples {
+    String dbUrl = "jdbc:oracle:thin:@3.86.27.247:1521:XE";  // 34.239.124.255
+    String dbUsername = "hr";
+    String dbPassword = "hr";
+
+    @Test
+    public void test1() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("Select * from departments");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString(1) + " - " + resultSet.getString(2)
+                    + " - " + resultSet.getString(3) + " - " + resultSet.getString(4));
+        }
+    }
+
+    @Test
+    public void test2() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("Select * from departments");
+
+        //how to find how many rows we have for the query
+
+        int count = 0;
+        while (resultSet.next()) {
+
+            resultSet.getRow();
+
+            count++;
+        }
+
+        System.out.println("Rows = " + count);
+
+        resultSet.last();
+        int rowCount = resultSet.getRow();
+        System.out.println(rowCount);
+
+        //print all second column info
+        resultSet.beforeFirst();
+        while(resultSet.next()) {
+            System.out.println(resultSet.getString(2));
+        }
+
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+
+
+    }
+    @Test
+    public void test3() throws SQLException {
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet resultSet = statement.executeQuery("Select * from departments");
+
+        //get the database related data inside the dbmetadata object
+
+        DatabaseMetaData dbMetaData = connection.getMetaData();
+
+        System.out.println("dbMetaData.getUserName() = " + dbMetaData.getUserName());
+        System.out.println("dbMetaData.getDatabaseProductName() = " + dbMetaData.getDatabaseProductName());
+        System.out.println("dbMetaData.getDatabaseProductVersion() = " + dbMetaData.getDatabaseProductVersion());
+        System.out.println("dbMetaData.getDriverName() = " + dbMetaData.getDriverName());
+
+
+        //get the resultSetMetadata
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+
+        int colCount = rsmd.getColumnCount();
+        System.out.println("colCount = " + colCount);
+
+        //getting column name
+        System.out.println("rsmd.getColumnName(1) = " + rsmd.getColumnName(1));
+
+        //print all the column names dynamically
+        System.out.println("===============================================");
+
+        for (int i = 1; i <= colCount; i++) {
+            System.out.println(rsmd.getColumnName(i));
+        }
+
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+
+
+
+
 }
+
+
+
